22410878e1b4085cacf604c93b42d2728c487551
jihoon098 <jihoon098@naver.com>
2024-06-09 13:41:52 +0900

    서블릿 컨테이너(Tomcat) 실행 코드 추가

--- src/main/java/hoonspring/helloboot/HellobootApplication.java
+++ src/main/java/hoonspring/helloboot/HellobootApplication.java
@@ -1,13 +1,22 @@
 package hoonspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		/* ServletWebServerFactory
+		 * : 서블릿 컨테이너의 종류에 종속되지 않도록 추상화한 인터페이스
+		 *  
+		 * Tomcat ServletWebServerFactory
+		 * : Spring에서 Tomcat 서블릿 컨테이너를 코드로 이용할 수 있도록 도와주는 클래스
+		 */
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		
+		WebServer webServer = serverFactory.getWebServer(); // 웹서버 생성
+		webServer.start(); // 웹서버(Tomcat 서블릿 컨테이너) 실행
 	}
 
 }
