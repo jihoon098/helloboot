55a54dad2f59438f7afc7438975d5f4827916a9f
jihoon098 <jihoon098@naver.com>
2024-06-19 10:16:15 +0900

    Dispatcher Servlet으로 서블릿 변경

--- src/main/java/hoonspring/helloboot/HellobootApplication.java
+++ src/main/java/hoonspring/helloboot/HellobootApplication.java
@@ -1,20 +1,10 @@
 package hoonspring.helloboot;
 
-import java.io.IOException;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
@@ -24,11 +14,11 @@ public class HellobootApplication {
 		 * 스프링 컨테이너 생성 & Bean 등록
 		 * ★ Application Context : 스프링 컨테이너를 대표하는 인터페이스 이름
 		 * 
-		 * GenericApplicationContext
-		 * : applicationContext(즉, 스프링 컨테이너)를 코드로 이용할 수 있도록 만들어진 클래스
+		 * GenericWebApplicationContext
+		 * : 기존 applicationContext에서 웹환경에서 쓰이도록 기능이 추가된 것.
 		 * 
 		 */
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		/* 
 		 * 스프링은 클래스의 메타 정보를 등록(register)하는 방식으로 Bean을 생성.
 		 * Assembler(= 스프링 컨테이너)는 런타임에 Bean 객체를 생성하며 동시에 의존관계를 맺어줌.
@@ -50,44 +40,18 @@ public class HellobootApplication {
 		// 웹서버 생성
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			
-			// 생성한 서블릿 컨테이너에 서블릿을 추가
-			servletContext.addServlet("Front Controller", new HttpServlet() {
-				/*
-				 * 서블릿내에 웹요청 및 응답에 대한 Object가 파라미터로 전달되는 것을 볼 수 있음.
-				 */
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					
-					/* 
-					 * 서블릿 컨테이너의 매핑 기능을 프론트 컨트롤러가 담당.
-					 * 매핑 : 요청 정보(HTTP Method, Request URI 등등)를 통해 진행됨.
-					 */
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						// 요청 Object 처리
-						String name = req.getParameter("name");
-						
-						/*
-						 * Front Controller는 직접적으로 웹요청과 엑세스하고
-						 * 웹기술과 관련된 Object들(상태값, 요청, Path 정보 등등)에게서
-						 * 평범한 데이터타입으로 전환하여 비지니스를 처리하는 Object에게 파라미터를 바인딩해줌.
-						 * 기존 컨트롤러는 직접적인 웹요청과 분리됨. 
-						 */
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-						
-						// 응답 Object 처리. Status를 set하지 않아도 서블릿 컨테이너는 기본적으로 에러가 나지않는 한 200번 상태코드 리턴.
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else if (req.getRequestURI().equals("/user")) {
-						System.out.println("user URI로 매핑");
-					}
-					else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value()); //404
-					}
-					
-				}
-			}).addMapping("/*"); // Front Controller 라는 개념은 중앙화된 처리를 위해 모든 요청을 다 받을 수 있도록 하는 것.
+			/* 
+			 * DispatcherServlet
+			 * : 프론트 컨트롤러의 많은 기능들(매핑 및 바인딩 등)을 수행하는 Servlet 클래스.
+			 * 
+			 * 1. Servlet이기 때문에 웹요청이 들어왔을때 해당 정보를 분석해 스프링 컨테이너가 관리하는 Object한테 요청을 넘겨줘야함.
+			 * 	  따라서 웹환경에서 사용할 수 있는 application Context가 필요한 것.
+			 * 2. DispatcherServlet에게 위 행동을 할 수 있도록 어떻게든 매핑 정보를 줘야하는데,
+			 * 	  매핑 정보를 Servlet 내에 코드로 넣는대신 그 요청을 처리할 Controller 클래스에 매핑 정보를 명시하는 방법(Annotation)을 가장 많이 씀.
+			 */
+			servletContext.addServlet("Dispatcher Servlet", new DispatcherServlet(applicationContext)
+			
+			).addMapping("/*");
 		});
 		
 		webServer.start(); // 웹서버(Tomcat 서블릿 컨테이너) 실행
