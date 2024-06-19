5146c391bf03f5a8731714e3ad4c9f709bcd86c2
jihoon098 <jihoon098@naver.com>
2024-06-19 13:50:48 +0900

    Controller클래스에 어노테이션으로 URI 매핑 적용

--- src/main/java/hoonspring/helloboot/HelloController.java
+++ src/main/java/hoonspring/helloboot/HelloController.java
@@ -2,6 +2,18 @@ package hoonspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*
+ * dispatcherServle은 먼저 클래스 레벨의 정보를 먼저 참고한 후, 메소드 레벨의 정보를 추가함.
+ * (hello URI를 클래스 레벨이 아닌 메소드들에게만 명시해도 당연히 됨)
+ */
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
 	// final은 정의할 때부터 초기화 or 생성자에서 초기화 코드가 필요
 	private final HelloServiceInf helloServiceInf;
@@ -10,7 +22,13 @@ public class HelloController {
 		this.helloServiceInf = helloServiceInf;
 	}
 	
-	public String hello(String name) {
+	/*
+	 * ★ dispatcherServlet의 기본 동작은 String이 return되면 View 이름으로 해석한다!!
+	 * 현재는 문자열의 return을 기대하기에 @ResponseBody 사용.
+	 */
+	@GetMapping
+	@ResponseBody
+	public String hello(@RequestParam("name") String name) {
 		
 		/*
 		 * Objects.requireNonNull <- Null 체크를 위한 메소드
