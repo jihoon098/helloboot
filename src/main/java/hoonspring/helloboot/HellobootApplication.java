d69ea2158da271559e5a9a7758d3b0c026faa592
jihoon098 <jihoon098@naver.com>
2024-06-21 10:04:27 +0900

    MySpringApplication.run

--- src/main/java/hoonspring/helloboot/HellobootApplication.java
+++ src/main/java/hoonspring/helloboot/HellobootApplication.java
@@ -1,12 +1,10 @@
 package hoonspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /*
@@ -28,40 +26,10 @@ public class HellobootApplication {
 	}
 	
 	public static void main(String[] args) {
-		
-		/*
-		 * AnnotationConfigWebApplicationContext
-		 * : Annotation이 붙은 Java코드로 만든 구성 정보(Configuration)를 읽을 수 있는 컨테이너
-		 */
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-				
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-				/*
-				 * DispatcherServlet에는 스프링(ApplicationContext)를 주입해 줘야 하는데, 아래 코드를 지워도 동작함.
-				 * 그 이유는 DispatcherServlet이 ApplicationContextAware이라는 인터페이스를 구현하고 있기 때문.
-				 * 스프링컨테이너는 ApplicationContextAware를 구현하고 있는 클래스에 자기 자신인 애플리케이션 컨텍스트를 주입한다.
-				 */
-				// dispatcherServlet.setApplicationContext(this);
-				
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("Dispatcher Servlet", dispatcherServlet
-					
-					).addMapping("/*");
-				});
-				
-				webServer.start(); // 웹서버(Tomcat 서블릿 컨테이너) 실행
-			}
-		};
-		
-		// Java코드로 된 구성(Config)정보를 가진 클래스를 ApplicationContext에 등록
-		applicationContext.register(HellobootApplication.class);
-		// 구성된 정보를 이용해 컨테이너를 초기화.
-		applicationContext.refresh();
-		
+		// 핵심 포인트: 파라미터로 주입하는 클래스는 '스프링 컨테이너의 구성정보를 가진 클래스'여야 한다.
+		MySpringApplication.run(HellobootApplication.class, args);
 	}
+	
+	
 
 }
