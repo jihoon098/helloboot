a18ce4886842ca9da04bf74535885048a9e13cdc
jihoon098 <jihoon098@naver.com>
2024-06-25 15:03:42 +0900

    메인 클래스에 Composed 어노테이션 적용 & 서블릿관련 팩토리 메소드 분리

--- src/main/java/hoonspring/helloboot/HellobootApplication.java
+++ src/main/java/hoonspring/helloboot/HellobootApplication.java
@@ -1,35 +1,11 @@
 package hoonspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-/*
- * 컴포넌트 스캔 -> @ComponentScan
- * : @Component라는 Annotation이 붙은 모든 클래스를 찾아서 빈으로 등록해주는 역할. 이때 DI도 발생한다.
- */
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class HellobootApplication {
-	
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-	
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
-	
-	
-
 }
