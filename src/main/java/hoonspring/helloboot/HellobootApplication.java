4252a93fa1e6cd69b56d7ca595304f920c6906ff
jihoon098 <jihoon098@naver.com>
2024-07-04 12:22:26 +0900

    ApplicationRunner 및 Environment 프로퍼티 활용

--- src/main/java/hoonspring/helloboot/HellobootApplication.java
+++ src/main/java/hoonspring/helloboot/HellobootApplication.java
@@ -1,11 +1,27 @@
 package hoonspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import hoonspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellobootApplication {
+	
+	@Bean
+	ApplicationRunner applicationRunner(Environment env) { // 컨테이너로부터 Environment 객체를 주입받음
+		return new ApplicationRunner() {
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+				String priority = env.getProperty("my.priority");
+				System.out.println("Environment 의 현재 우선순위는 " + priority);
+			}
+		};
+	}
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
