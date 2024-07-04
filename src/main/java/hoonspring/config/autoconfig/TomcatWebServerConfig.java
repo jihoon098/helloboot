a93c0ea6a988935017e089adc1cf50f46b4a139b
jihoon098 <jihoon098@naver.com>
2024-07-04 13:46:44 +0900

    자동구성 코드안에 외부 프로퍼티 적용(Tomcat 팩토리에 contextPath 프로퍼티 적용)

--- src/main/java/hoonspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/hoonspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import hoonspring.config.ConditionalMyOnClass;
 import hoonspring.config.MyAutoConfiguration;
@@ -12,15 +13,12 @@ import hoonspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 	
-	/*
-	 * ConditionalOnMissingBean
-	 * : 동일한 타입의 빈이 이미 존재하는가?를 체크하고 존재하지 않으면 빈으로 만듬.
-	 *   즉, 사용자가 직접 등록한 빈이 있는지를 확인한 후에, 자동구성 빈을 생성할지 판단
-	 */
 	@ConditionalOnMissingBean
 	@Bean("tomcatWebServerFactory")
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
+	public ServletWebServerFactory servletWebServerFactory(Environment env) {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.setContextPath(env.getProperty("contextPath"));
+		return factory;
 	}
 	
 }
--- src/test/java/hoonspring/helloboot/HelloApiTest.java
+++ src/test/java/hoonspring/helloboot/HelloApiTest.java
@@ -14,15 +14,13 @@ public class HelloApiTest {
 	@Test
 	void HelloApi() {
 		/*
-		 * http localhost:8080/hello?name=Spring
-		 * 
-		 * RestTemplate : 위 HTTP형식의 Api요청을 코드로 해주도록 스프링이 만들어준 클래스
+		 * RestTemplate : HTTP형식의 Api요청을 코드로 해주도록 스프링이 만들어준 클래스
 		 * -> 해당 클래스를 이용하면 API요청 및 응답을 받을 수 있음. 정상적인 응답의 경우는 문제없지만 400번때나 500번때 에러가 발생하면 예외를 던짐.
 		 * 물론 예외처리를 하면되지만. 그냥 응답 자체를 전부 가져와서 상태코드, 컨텐츠 등을 파악하려면 TestRestTemplate클래스가 훨씬 편리함.
 		 */
 		TestRestTemplate rest = new TestRestTemplate();
 		// ResponseEntity : 웹응답의 모든 요소를 다 가지고 있는 오브젝트. 그리고 <String>은 body가 String이라는 의미.
-		ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+		ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
 
 		// Assertion 검증 라이브러리 사용
 		// 웹응답 3가지 요소 검증
